9b246bb5d5315d08c4130bd090f1a5407b1d28ce
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2016-02-20 19:45:49 +0100

    update project license and url

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject kpar "0.1.0-SNAPSHOT"
   :description "Parallel combinators for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/kikofernandez/ParT"
   :license {:name "MIT"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.nrepl "0.2.12"]]
   :profiles {:dev {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
